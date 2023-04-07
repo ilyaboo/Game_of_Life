6b90134cfd277c9ff3f66d01dd4208a50a107cbd
ilyaboo <ibouligin@gmail.com>
2023-04-07 04:25:16 -0400

    added the grid preview

--- main.c
+++ main.c
@@ -2,23 +2,25 @@
 #include "gfx.h"
 
 int main() {
-	int ysize = 800;
-	int xsize = 800;
-
-	char c;
-
-	// Open a new window for drawing.
+	int ysize = 820;
+	int xsize = 820;
 	gfx_open(xsize,ysize,"Example Graphics Program");
+	gfx_color(0,200,100);	
+	
+	// initializing field with zeros
+	int field[10][10];
+	
 
-	// Set the current drawing color to green.
-	gfx_color(0,200,100);
 
-	// Draw a triangle on the screen.
-	gfx_line(100,100,200,100);
-	gfx_line(200,100,150,150);
-	gfx_line(150,150,100,100);
-
-	gfx_color(200,20,100);
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			gfx_line(80 * j + 10, 80 * i + 10, 80 * j + 10 + 80, 80 * i + 10);
+			gfx_line(80 * j + 10 + 80, 80 * i + 10, 80 * j + 10 + 80, 80 * i + 10 + 80);
+			gfx_line(80 * j + 10 + 80, 80 * i + 10 + 80, 80 * j + 10, 80 * i + 10 + 80);
+			gfx_line(80 * j + 10, 80 * i + 10 + 80, 80 * j + 10, 80 * i + 10);
+		}
+	}
+	char c;
 	while(1) {
 		// Wait for the user to press a character.
 		c = gfx_wait();
