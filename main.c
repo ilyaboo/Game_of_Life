dd19e0e637da417480b5799d3a3e63d7b373633e
ilyaboo <ibouligin@gmail.com>
2023-04-07 16:15:35 -0400

    added functionality for field matrix as well as display function for the field matrix

--- main.c
+++ main.c
@@ -2,10 +2,9 @@
 #include "gfx.h"
 
 int num_squares = 10;   // number of sqaures in a row/column
-static int side = 80;   // length of the side of one square
+static int side = 40;   // length of the side of one square
 int margin = 10;   // margin between the field and edge of the window
-	   
-
+int field[10][10];
 
 void gen_grid(int num_squares, int side, int margin) {
 	// function that generates a grid for the game field
@@ -39,21 +38,45 @@ void fill_square_at_location(int row, int col) {
 	fill_square(margin + row * side, margin + col * side);
 }
 
+void empty_field() {
+	// function that sets all values of the field matrix to zero
+	for (int row = 0; row < num_squares; ++row) {
+		for (int col = 0; col < num_squares; ++col) {
+			field[row][col] = 0;
+		}
+	}
+}
+
+void gen_board() {
+	// function that inspects the contents of field matrix
+	// and generates a visual representation of it on the game field
+	for (int row = 0; row < num_squares; ++row) {
+		for (int col = 0; col < num_squares; ++col) {
+			// printf("row: %d, col: %d \n", row, col);
+			if (field[row][col] == 1) {
+				fill_square_at_location(row, col);
+			}
+		}
+	}
+}
+
 int main() {
 	int ysize = margin * 2 + side * num_squares;   // vertical size of the window
 	int xsize = margin * 2 + side * num_squares;   // horizontal size of the window
-	gfx_open(xsize,ysize,"Example Graphics Program");
-	gfx_color(0,200,100);	
+	gfx_open(xsize, ysize, "Game of Life");
+	gfx_color(0, 200, 100);	
 	
 	// initializing field with zeros
-	int field[10][10];
-
+	empty_field();
+	field[1][1] = 1;
+	field[2][3] = 1;
+	field[0][0] = 1;
+	field[9][9] = 1;
+	
 	// generating the grid for the field
 	gen_grid(num_squares, side, margin);
+	gen_board();
 
-	fill_square_at_location(0, 0);
-	fill_square_at_location(1, 1);
-	fill_square_at_location(9, 9);
 	char c;
 	while(1) {
 		// Wait for the user to press a character.
