8b4d20833d540b4451dedf0f72bf9319d04e69ac
ilyaboo <ibouligin@gmail.com>
2023-04-11 13:42:25 -0400

    updated the code to work with earlier versions of gcc compiler

--- main.c
+++ main.c
@@ -13,29 +13,29 @@
 ///////////////////////////////////////
 
 // general game parameters
-const int max_epochs = 50;   // the maximum number of epochs after which the program stops, default: 50
-const int num_squares = 40;   // number of sqaures in a row/column, default: 40
-const int sleep_time = 4 * 100000;   // time for refreshing the field in microseconds, default: 0.4s
+#define max_epochs 50   // the maximum number of epochs after which the program stops, default: 50
+#define num_squares 40   // number of sqaures in a row/column, default: 40
+#define sleep_time 4 * 100000   // time for refreshing the field in microseconds, default: 0.4s
 
 // display parameters
-const int side = 15;   // length of the side of one square, default: 15
-const int margin = 10;   // margin between the field and edge of the window, default: 10
+#define side 15   // length of the side of one square, default: 15
+#define margin 10   // margin between the field and edge of the window, default: 10
 
 // display parameters for buttons
-const int start_button_width = 3 * side;   // width of the start button (multiple of side of the square)
-const int start_button_height = 1 * side;   // height of the start button (multiple of side of the square)
-const int start_button_x = margin * 2 + side * num_squares;   // x position of the top left corner of the start button
-const int start_button_y = margin + side;   // y position of the top left corner of the start button
-const int reset_button_width = 3 * side;   // width of the reset button (multiple of side of the square)
-const int reset_button_height = 1 * side;   // height of the reset button (multiple of side of the square)
-const int reset_button_x = margin * 2 + side * num_squares;   // x position of the top left corner of the reset button
-const int reset_button_y = margin + 3 * side;   // y position of the top left corner of the reset button
-const int preset_button_width = 3 * side;   // width of the preset buttons (multiple of side of the square)
-const int preset_button_height = 1 * side;   // height of the preset buttons (multiple of side of the square)
-const int preset_button_x = margin * 2 + side * num_squares;   // x position of the top left corner of the preset buttons
-const int preset1_button_y = margin + 5 * side;   // y position of the top left corner of the first preset button
-const int preset2_button_y = margin + 7 * side;   // y position of the top left corner of the second preset button
-const int preset3_button_y = margin + 9 * side;   // y position of the top left corner of the third preset button
+#define start_button_width 3 * side   // width of the start button (multiple of side of the square)
+#define start_button_height side   // height of the start button (multiple of side of the square)
+#define start_button_x  2 * margin + side * num_squares   // x position of the top left corner of the start button
+#define start_button_y margin + side   // y position of the top left corner of the start button
+#define reset_button_width 3 * side   // width of the reset button (multiple of side of the square)
+#define reset_button_height side   // height of the reset button (multiple of side of the square)
+#define reset_button_x margin * 2 + side * num_squares   // x position of the top left corner of the reset button
+#define reset_button_y margin + 3 * side   // y position of the top left corner of the reset button
+#define preset_button_width 3 * side   // width of the preset buttons (multiple of side of the square)
+#define preset_button_height side   // height of the preset buttons (multiple of side of the square)
+#define preset_button_x margin * 2 + side * num_squares   // x position of the top left corner of the preset buttons
+#define preset1_button_y margin + 5 * side   // y position of the top left corner of the first preset button
+#define preset2_button_y margin + 7 * side   // y position of the top left corner of the second preset button
+#define preset3_button_y margin + 9 * side   // y position of the top left corner of the third preset button
 int field[num_squares][num_squares];
 
 
@@ -44,7 +44,7 @@ int field[num_squares][num_squares];
 //////// INTERFACE FUNCTIONS //////////
 ///////////////////////////////////////
 
-void gen_grid(int num_squares, int side, int margin) {
+void gen_grid() {
 	// function that generates a grid for the game field
 	for (int i = 0; i < num_squares; ++i) {
 		for (int j = 0; j < num_squares; ++j) {
@@ -128,7 +128,7 @@ void update_all() {
 	// function that updates all the interface according to the current
 	// state of the field
 	gfx_clear();
-	gen_grid(num_squares, side, margin);
+	gen_grid();
 	gen_board();
 	gen_start_button();
 	gen_reset_button();
